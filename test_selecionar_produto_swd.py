578e84b995588790a088f42ecd3a783b329db9ec
RobertaCris <robertacgsilva@hotmail.com>
2024-03-13 22:11:21 -0300

    validação do primeiro iten, preço

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -13,12 +13,17 @@ class Teste_Produtos():
     # 2.2 Funções e Métodos
     def setup_method(self, method):            # método de incialização dos testes
         self.driver = webdriver.Chrome()       # instancia o objeto do Selenium Webdriver como Chrome
-        self.driver.implicitly_wait(10)          # define o tempo de espera padrão por elementos em 10 segundos 
+        self.driver.implicitly_wait(10)        # define o tempo de espera padrão por elementos em 10 segundos 
 
         def teardown_method(self, method):     # metodo de finalização dos testes 
             self.driver.quit()                 # encerra / destrói o objeto do Selenium Webdriver da memória
 
-    def test_selecionar_produto(self): # método de teste
-        self.driver.get(self.url)              # abre o navegador
-        self.driver.find_element(By.ID,"user-name").send_keys("standard_user") # escreve no campo user-name
-        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce") # escreve a senha       
+    def test_selecionar_produto(self):         # método de teste
+            self.driver.get(self.url)              # abre o navegador
+            self.driver.find_element(By.ID,"user-name").send_keys("standard_user")       # escreve no campo user-name
+            self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")      # escreve a senha
+            self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click()  # clique no botão de login
+            assert self.driver.find_element(By.CSS_SELECTOR, "span.title").text == "Products"   # Confirma se esta escrito Products no elemento
+            assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack" # Confirma se é a mochila
+            # confirma preço da mochila
+            assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
